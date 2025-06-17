aae532493b859fe33fdeab68ee1d4546a1dfa545
tiendaenofertas <tiendaenofertas@gmail.com>
2025-06-17 14:08:25 -0500

    Delete includes directory

--- includes/class-eroz-deactivator.php
+++
@@ -1,5 +0,0 @@
-<?php
-class EROZ_Deactivator {	
-    public static function deactivate(){
-    }
-}
--- includes/class-eroz-support.php
+++
@@ -1,7 +0,0 @@
-<?php
-Class EROZ_Support {
-	public function code_analityc(){
-		$code     = get_option( 'analityc_code', false );
-        echo $code;
-	}
-}
