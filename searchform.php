9c428529fd440b842d7101baefb9d789f4804d6a
tiendaenofertas <tiendaenofertas@gmail.com>
2026-05-08 13:23:28 -0500

    Delete searchform.php

--- searchform.php
+++
@@ -1,4 +0,0 @@
- <form method="get" action="<?php echo get_home_url(); ?>">
-    <input name="s" type="text" placeholder="<?php echo lang_eroz('Search...', 'lang_search'); ?>">
-    <button type="submit"><i class="fa-search"></i></button>
-</form>
